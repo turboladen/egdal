ad853402ab38f82d3b376c8f63e1b99257651dee
Steve Loveless <steve.loveless@gmail.com>
2016-04-05 00:07:29 -0700

    Add exdocs

--- lib/egdal/gdal/data_type.ex
+++ lib/egdal/gdal/data_type.ex
@@ -5,19 +5,57 @@ defmodule Egdal.GDAL.DataType do
     :ok = :erlang.load_nif('./egdal_gdal_data_type', 0)
   end
 
+  @doc """
+  Gets the DataType using the shortened `name`.
+
+  * Parameters
+    * _name: Atom that represents the type name.
+  * Returns a tuple
+    * *Atom* `:ok` or `:error`
+    * *Integer* equivalent to the GDALDataType enum value.
+  """
   def by_name(_name) do
-    "NIF not loaded"
+    {:error, "NIF not loaded"}
   end
 
+  @doc """
+  Gets the DataType using the shortened `name`.
+
+  * Parameters
+    * _index: *Integer* equal to an GDALDataType enum value.
+  * Returns a tuple
+    * *Atom* `:ok` or `:error`
+    * *Atom* equivalent to the GDALDataType name.
+  """
   def name(_index) do
-    "NIF not loaded"
+    {:error, "NIF not loaded"}
   end
 
+  @doc """
+  Gets the size (in bits) of the DataType using the shortened `name`.
+
+  * Parameters
+    * _name: Atom that represents the type name.
+  * Returns a tuple
+    * *Atom* `:ok` or `:error`
+    * *Integer* of the GDALDataType size.
+  """
   def size(_name) do
-    "NIF not loaded"
+    {:error, "NIF not loaded"}
   end
 
+  @doc """
+  Gets the size (in bits) of the DataType using the shortened `name`.
+
+  * Parameters
+    * _name: Atom that represents the type name.
+  * Returns a tuple
+    * *Atom* `:ok` or `:error`
+    * *Atom*, either `:true` or `:false`.
+  """
   def complex?(_name) do
-    "NIF not loaded"
+    {:error, "NIF not loaded"}
+  end
+
   end
 end
