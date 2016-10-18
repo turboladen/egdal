c134a9a674b65107d886a03960287dd472637c28
Steve Loveless <steve.loveless@gmail.com>
2016-10-17 22:26:02 -0700

    Trying without rust-gdal
    rust-gdal preps data for rust (obviously), which means I have to undo
    some of that to return for erlang.

--- native/gdal/src/lib.rs
+++ native/gdal/src/lib.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate rustler;
-extern crate gdal;
 
-use rustler::{NifEnv, NifTerm, NifError, NifDecoder, NifEncoder, NifResult};
-use rustler::resource::ResourceCell;
-use rustler::atom::{init_atom, get_atom};
-use rustler::tuple::make_tuple;
+use std::ffi::CString;
+use std::ffi::CStr;
+use std::str;
+use std::os::raw::c_char;
+
+use rustler::{NifEnv, NifTerm, NifResult};
 use rustler::binary::OwnedNifBinary;
-use gdal::version::version_info;
 
 use std::io::Write;
 
@@ -17,15 +17,28 @@ rustler_export_nifs!(
     Some(on_load)
 );
 
-fn on_load(env: &NifEnv, load_info: NifTerm) -> bool {
+fn on_load(_env: &NifEnv, _load_info: NifTerm) -> bool {
     true
 }
 
-fn get_version_info<'a>(env: &'a NifEnv, args: &Vec<NifTerm>) -> NifResult<NifTerm<'a>> {
-    let psz_request = "RELEASE_NAME";
-    let version = gdal::version::version_info("RELEASE_NAME");
-    let mut binary = OwnedNifBinary::alloc(version.len()).unwrap();
-    binary.as_mut_slice().write_all(version.as_bytes());
+#[link(name="gdal")]
+extern {
+    fn GDALVersionInfo(key: *const c_char) -> *const c_char;
+}
+
+fn get_version_info<'a>(env: &'a NifEnv, _args: &Vec<NifTerm>) -> NifResult<NifTerm<'a>> {
+    let key = "RELEASE_NAME";
+    let c_key = CString::new(key.as_bytes()).unwrap();
+
+    let c_str = unsafe {
+        let version_ptr = GDALVersionInfo(c_key.as_ptr());
+        CStr::from_ptr(version_ptr)
+    };
+
+    let meow = str::from_utf8(c_str.to_bytes()).unwrap();
+
+    let mut binary = OwnedNifBinary::alloc(meow.len()).unwrap();
+    let _result = binary.as_mut_slice().write_all(meow.as_bytes());
 
     Ok(binary.release(env).get_term(env))
 }
